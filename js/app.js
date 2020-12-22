77e62dac245fd03f31ca6601c1cc16a8e69f00f8
pedro nin <pedro1996616@gmail.com>
2020-12-22 12:26:59 +0100

    acabado

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@
     const customerName = document.querySelector('#customer-name')
     const customerText = document.querySelector('#customer-text')
     const buttons = document.querySelectorAll('.btn')
+    const btnright= document.querySelector('.nextBtn')
+    const btnleft =  document.querySelector('.prevBtn')
     let index = 0
     const customers = []
 
@@ -31,5 +33,28 @@
     createCustomer(3, 'Tyrell', 'If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.')
     createCustomer(4, 'Wanda', 'Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.')
     
+    function nextContain (e){
+      console.log(e)
+      if (e.target.id == ("nextBtn")){
+          if(index >= customers.length-1){
+            index = 0
+          }
+          customerImage.src = customers[index].img
+          customerName.textContent = customers[index].name
+          customerText.textContent = customers[index].text
+      }else{
+        if(index <= 0){
+          index = customers.length-1
+        }
+        index--
+        customerImage.src = customers[index].img
+        customerName.textContent = customers[index].name
+        customerText.textContent = customers[index].text
+      }
+
+    }
+    btnleft.addEventListener('click',nextContain);
+    btnright.addEventListener('click',nextContain);
+
     // Fíjate bien en la estructura de este array de objetos. Identifica cuantas posiciones tiene y el nombre de cada una de sus propiedades
     console.log(customers)
